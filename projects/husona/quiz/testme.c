f15786ab6179264f91027e9c930a78f5e83615d8
Andrew <husona@oregonstate.edu>
2019-07-21 16:39:16 -0700

    code for quiz

---
+++ projects/husona/quiz/testme.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    return ((char)((rand() % 95) + 32));	// return a random character from ' ' (32) to '~' (126)
+}
+
+char *inputString()
+{
+	static char Alpha[5] = {'r','e','s','e','t'};	// function will randomly choose letters from this array
+	static char myString[6] = {'\0','\0','\0','\0','\0','\0'};
+	
+	int i;
+	for(i = 0; i < 5; i++)	// maintain myString as a null terminated string, never change myString[5]
+	{
+		myString[i] =  Alpha[((rand()) % 5)];	// assign a random letter from Alpha to myString
+	}
+
+    return myString;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
